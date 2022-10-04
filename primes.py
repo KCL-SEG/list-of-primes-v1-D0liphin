ad3449c539901652d21a77bdf99f8e5111c7bb6b
Oli Iliffe <oliver.iliffe@kcl.ac.uk>
2022-10-04 22:12:58 +0100

    add primes function body

--- primes.py
+++ primes.py
@@ -1,6 +1,17 @@
-"""List of prime numbers generator."""
-"""ENTER YOUR SOLUTION HERE!"""
-
-def primes(number_of_primes):
-    list = []
-    return list
+def primes(number_of_primes:int) -> list[int]:
+    """
+    generates the first `number_of_primes` primes and returns them as 
+    a list
+    """
+    primes = [2]
+    for _ in range(number_of_primes - 1):
+        tmp = primes[-1]
+        while True:
+            tmp += 1
+            for prime in primes:
+                if tmp % prime == 0:
+                    break
+            else:
+                primes.append(tmp)
+                break
+    return primes
